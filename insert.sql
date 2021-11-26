66e8a1466a9054f3131dc18a117760151669383c
MCMike0399 <maquinterov0399@gmail.com>
2021-11-25 23:35:04 -0600

    inserts

--- insert.sql
+++ insert.sql
@@ -1,21 +1,15 @@
-INSERT INTO frecuencia_uso (id_frecuencia_uso,redes_sociales,compras_online,streaming) VALUES
-	 (1,'Muy frecuente','Muy frecuente','Ni mucho ni poco frecuente'),
-	 (2,'Poco frecuente','Poco frecuente','Poco frecuente');
-INSERT INTO public.seguridad (id_seguridad,sentimiento_proteccion,conocimiento_ciberseg,conocimiento_vigilancia,conocimiento_leyes,informacion_sensible) VALUES
-	 (1,'No','Sí','Sí','Sí','Nombre y apellidos, Dirección de casa u oficina, Información bancaria'),
-	 (2,'Sí','Sí','Sí','Sí','Nombre y apellidos, Correo electrónico, Redes sociales, Información bancaria');
-INSERT INTO public.confianza_paginas (id_confianza_paginas,nombre,direccion,datos_bancarios,preferencias) VALUES
-	 (1,'Ni confío ni desconfío','Ni confío ni desconfío','Ni confío ni desconfío','Ni confío ni desconfío'),
-	 (2,'Desconfío','Desconfío','Desconfío','Desconfío');
-INSERT INTO public.compras_online (id_compras_online,tipo_compras,compras_uso,frecuencia_compras,formas_pago,id_confianza_paginas,mayor_miedo,publicidad_personalizada) VALUES
-	 (1,'Servicios de delivery (Rappi, Uber Eats, etc.), Otro','','De 2 a 3 veces a la semana','Tarjeta de crédito, Tarjeta de débito',1,'Robo de información bancaria','Personalizada'),
-	 (2,'Servicios de delivery (Rappi, Uber Eats, etc.), Suscripciones a servicios digitales','','De 2 a 3 veces a la semana','Tarjeta de crédito, Tarjetas prepago',2,'Mala calidad de los productos','Aleatoria');
-INSERT INTO public.encuestado (id_encuestado,edad,licenciatura,genero,dispositivos,id_frecuencia_uso,valoracion_priv_ef,actitud_ciberseg,compras_en_linea) VALUES
-	 (1,'20-22','Sí','Hombre','Laptop, Celular',1,'Intento encontrar un balance','Prudente y moderado','Sí'),
-	 (2,'20-22','Sí','No binario','Laptop, Tablet, Celular, Computadora de escritorio',2,'Eficiencia','Prudente y moderado','Sí');
-INSERT INTO public.redes_sociales (id_red_social,id_encuestado,redes_uso,tiempo_redes,razon_uso,contenido_compartido,perfiles_publicos,interaccion_desconocidos,acceso_desconocidos,herramientas_privacidad,servicios_personalizados,contenido_sin_vigilancia,contenido_observado,sentimientos,impacto_redes_vida,uso_stalkeo,comparacion_redes) VALUES
-	 (1,1,'Redes convencionales (Facebook, Twitter, Reddit, Linkedln), Redes de comunicación (Whatsapp, Telegram, Discord), Club Penguin','3 horas - 5 horas','Entretenimiento','Memes, Música y videos populares, Ideas propias','Sí','No','Sí','No','Depende de mi estado de ánimo al momento','Ni de acuerdo ni desacuerdo','Sí','Feliz, Triste, Enojado','Indistinto.','En ningún caso es correcto investigar sobre los datos personales de otros','Una herramienta'),
-	 (2,2,'Redes multimedia (Snapchat, TikTok), Apps de citas (Tinder, Grindr, Bumble), Club Penguin','Mayor a 8 horas','Trabajo','Fotos personales, Ideas propias, Información útil para los demás','No','No','Sí','Sí','Busco el contenido que me interesa','Ni de acuerdo ni desacuerdo','No','Feliz, Enojado, Indiferente','Negativo. Empeoran mi vida.','En ningún caso es correcto investigar sobre los datos personales de otros','Una herramienta');
-INSERT INTO public.conclusion (id_conclusion,id_encuestado,actitud_ciberseg,vigilancia,aprender_ciberseg) VALUES
-	 (2,2,'Prudente y moderado','Sí','Sí'),
-	 (1,1,'Prudente y moderado','Sí','Sí');
+insert into frecuencia_uso(id_frecuencia_uso,redes_sociales,compras_online,streaming) values(1,'Muy Frecuente','Frecuente','Poco Frecuente');
+insert into informacion_sensible(id_informacion_sensible,nombre,direccion,telefono,email,redes_sociales,info_bancaria,info_preferencias) values(1,false,true,false,false,false,true,false);
+insert into tipo_compras(id_tipo_compras,ropa,despensa,delivery,suscripciones,dispositivos_electronicos,otros) values(1,true,false,false,false,false,false);
+insert into forma_pago(id_forma_pago,credito,debito,prepago,paypal) values(1,true,true,false,false);
+insert into sentimientos_post_uso(id_sentimientos_post_uso,feliz,triste,enojado,indiferente,nostalgico,celoso,amenazado) values(1,false,false,true,true,false,false,false);
+insert into uso_stalkeo(id_uso_stalkeo,escuela,empleador,producto,persona,nunca) values(1,false,true,false,false,true);
+insert into contenido_compartido(id_contenido_compartido, memes,musica,fotos,ideas,info_util,nada) values(1,false,true,false,true,true,false);
+insert into redes_uso(id_redes_uso,convencionales,multimedia,comunicacion,citas,penguin,ninguna) values(1,false,true,false,false,false,true);
+insert into dispositivos(id_dispositivos,laptop,tablet,celular,computadora_escritorio) values(1,true,true,true,true);
+INSERT INTO confianza_paginas (id_confianza_paginas,nombre,direccion,datos_bancarios,preferencias) VALUES(1,'Ni confío ni desconfío','Ni confío ni desconfío','Ni confío ni desconfío','Ni confío ni desconfío');
+INSERT INTO encuestado (id_encuestado,edad,licenciatura,genero,id_dispositivos,id_frecuencia_uso,valoracion_priv_ef,actitud_ciberseg,compras_en_linea) VALUES(1,'20-22','Sí','Hombre',1,1,'Intento encontrar un balance','Prudente y moderado','Sí');
+INSERT INTO seguridad (id_seguridad,sentimiento_proteccion,conocimiento_ciberseg,conocimiento_vigilancia,conocimiento_leyes,id_informacion_sensible) VALUES(1,'No','Sí','Sí','Sí',1);
+INSERT INTO conclusion (id_conclusion,id_encuestado,actitud_ciberseg,vigilancia,aprender_ciberseg) VALUES(1,1,'Prudente y moderado','Sí','Sí');
+INSERT INTO compras_online (id_compras_online,id_encuestado,id_tipo_compras,compras_uso,frecuencia_compras,id_forma_pago,id_confianza_paginas,mayor_miedo,publicidad_personalizada) VALUES(1,1,1,'Entretenimiento','De 2 a 3 veces a la semana',1,1,'Robo de información bancaria','Personalizada');
+insert into redes_sociales(id_red_social,id_encuestado,id_redes_uso,tiempo_redes,razon_uso,id_contenido_compartido,perfiles_publicos,interaccion_desconocidos,acceso_desconocidos,herramientas_privacidad,servicios_personalizados,contenido_sin_vigilancia,contenido_observado,id_sentimientos_post_uso,impacto_redes_vida,id_uso_stalkeo,comparacion_redes) values(1,1,1,'3 horas - 5 horas','Entretenimiento',1,'Sí','No','Sí','No','Depende de mi estado de ánimo al momento','Ni de acuerdo ni desacuerdo','Sí',1,'Indistinto.',1,'Una herramienta');
